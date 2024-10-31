bcb78821e1a36888b57de913cea8f997220b4dd7
Lucas Balduino <lucasbalduino2@gmail.com>
2024-10-31 17:25:53 -0300

    primeiroCommit

---
+++ Models/Pessoa.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.Models
+{
+    public class Pessoa
+    {
+
+        public Pessoa()
+        {
+
+        }
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+        private string _nome;
+        private int _idade;
+        //fazendo o nome não podendo ser vazio 
+        public string Nome {
+            get => _nome.ToUpper();
+                 
+            set{
+                if(value == ""){ 
+                    throw new ArgumentException("O nome não pode ser nulo");
+                }
+
+                _nome = value;
+            } 
+        }
+
+        public string Sobrenome { get; set; }
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+
+            //fazendo a idade não podendo ser menor que 0
+        public int Idade {
+            get => _idade;
+            set{
+                if(value < 0){
+                    throw new ArgumentException("Idade não pode ser menor que 0");
+                }
+
+                _idade = value;
+            }
+            
+            }
+
+        public void Apresentar(){
+            Console.WriteLine($"Nome: {NomeCompleto}, Idade: {Idade}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using ExemploExplorando.Models;
+
+
+Pessoa p1 = new Pessoa(nome:"Lucas",sobrenome: "Balduino");
+Pessoa p2 = new Pessoa(nome: "Rogerio",sobrenome: "Fernandes");
+
+
+
+Curso cursoDeingles = new Curso();
+cursoDeingles.Nome = "Inglês";
+cursoDeingles.Alunos = new List<Pessoa>();
+
+cursoDeingles.AdicionarAluno(p1);
+cursoDeingles.AdicionarAluno(p2);
+cursoDeingles.ListarAlunos();
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+
+// p1.Nome = "Lucas";
+// p1.Sobrenome = "Balduino";
+// p1.Idade = 20;
+
+// p1.Apresentar();
